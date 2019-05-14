a3cb02dcf6366b6fd0ac977c629205e10a63ca2c
Frederik Jensen <kungfu15447@users.noreply.github.com>
2019-05-14 12:26:37 +0200

    Tilføjet json fil og lavet massere af interfaces og klasser

---
+++ Endgame/src/endgame/DAL/json/JSONOrder.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package endgame.DAL.json;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Frederik Jensen
+ */
+public class JSONOrder implements IOrder
+{
+
+    @Override
+    public Date getDeliveryTime()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getOrderNumber()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getCustomer()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ Endgame/src/endgame/DAL/json/JSONWorker.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package endgame.DAL.json;
+
+/**
+ *
+ * @author Frederik Jensen
+ */
+public class JSONWorker implements IWorker
+{
+
+    @Override
+    public String getName()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getInitials()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getSalaryNumber()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
