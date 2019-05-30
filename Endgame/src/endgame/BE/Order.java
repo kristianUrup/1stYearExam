74a5e918c38a3c91fe39604afa6f5427cb7169da
Jørgen H <j_wannerhenriksen@hotmail.com>
2019-05-30 13:57:12 +0200

    BE JavaDoc

--- Endgame/src/endgame/BE/Order.java
+++ Endgame/src/endgame/BE/Order.java
@@ -28,7 +28,16 @@ public class Order
     private Date deliveryDate;
 
     
-    
+    /**
+     *  constructs an order with an id, an ordernumber, a customer, a startdate, an enddate, a condition and a deliverydate
+     * @param id
+     * @param orderNumber
+     * @param customer
+     * @param startDate
+     * @param endDate
+     * @param condition
+     * @param deliveryDate 
+     */
     public Order(int id, String orderNumber, String customer, Date startDate, Date endDate, String condition, Date deliveryDate)
     {
         this.ID = new SimpleIntegerProperty(id);
@@ -39,7 +48,13 @@ public class Order
         this.endDate = endDate;
         this.deliveryDate = deliveryDate;
     }
-    
+    /**
+     * constructs an order with an id, an ordernumber a customer and a deliverydate
+     * @param id
+     * @param orderNumber
+     * @param customer
+     * @param deliveryDate 
+     */
     public Order(int id, String orderNumber, String customer, Date deliveryDate) {
         this.ID = new SimpleIntegerProperty(id);
         this.deliveryDate = deliveryDate;
@@ -47,40 +62,72 @@ public class Order
         this.customer = new SimpleStringProperty(customer);
     }
 
+    /**
+     * Gets the order's id
+     * @return 
+     */
     public int getId() {
         return ID.get();
     }
     
+    /**
+     * Gets the order's ordernumber
+     * @return 
+     */
     public String getOrderNumber()
     {
         return orderNumber.get();
     }
 
+    /**
+     * Gets the customer of the order
+     * @return 
+     */
     public String getCustomer()
     {
         return customer.get();
     }
 
+    /**
+     * Gets the startdate of the order
+     * @return 
+     */
     public Date getStartDate()
     {
         return startDate;
     }
 
+    /**
+     * Gets the Enddate of the order
+     * @return 
+     */
     public Date getEndDate()
     {
         return endDate;
     }
 
+    /**
+     * Gets the condition of the order
+     * @return 
+     */
     public String getCondition()
     {
         return condition.get();
     }
 
+    /**
+     * Gets the deliverydate of the order
+     * @return 
+     */
     public Date getDeliveryDate()
     {
         return deliveryDate;
     }
     
+    /**
+     * Sets the condition of the order as an StringProperty
+     * @param condition 
+     */
     public void setCondition(StringProperty condition)
     {
         this.condition = condition;
--- Endgame/src/endgame/BE/Worker.java
+++ Endgame/src/endgame/BE/Worker.java
@@ -21,6 +21,13 @@ public class Worker
     private IntegerProperty salaryNumber;
     private StringProperty initials;
 
+    /**
+     * constructs a worker with an id, a name, a salarynumber and initials
+     * @param id
+     * @param name
+     * @param salaryNumber
+     * @param initials 
+     */
     public Worker(int id, String name, int salaryNumber, String initials)
     {
         this.id = new SimpleIntegerProperty(id);
@@ -29,46 +36,81 @@ public class Worker
         this.initials = new SimpleStringProperty(initials);
     }
 
+    /**
+     * Gets the workers' id
+     * @return 
+     */
     public int getId()
     {
         return id.get();
     }
-
+    /**
+     * Sets the workers id
+     * @param id 
+     */
     public void setId(IntegerProperty id)
     {
         this.id = id;
     }
 
+    /**
+     * Gets the workers' name
+     * @return 
+     */
     public String getName()
     {
         return name.get();
     }
 
+    /**
+     * Sets the worker's name
+     * @param name 
+     */
     public void setName(StringProperty name)
     {
         this.name = name;
     }
 
+    /**
+     * Gets the worker's salary number
+     * @return 
+     */
     public int getSalaryNumber()
     {
         return salaryNumber.get();
     }
 
+    /**
+     * Sets the worker's salarynumber as an IntegerProperty
+     * @param salaryNumber 
+     */
     public void setSalaryNumber(IntegerProperty salaryNumber)
     {
         this.salaryNumber = salaryNumber;
     }
-
+    
+    /**
+     * Gets the workers' initials
+     * @return 
+     */
     public String getInitials()
     {
         return initials.get();
     }
 
+    /**
+     * Sets the worker's initials as an StringProperty
+     * @param initials 
+     */
     public void setInitials(StringProperty initials)
     {
         this.initials = initials;
     }
     
+    /**
+     * Gets the workers' salarynumber in an IntegerProperty 
+     * @return 
+     */
     public IntegerProperty getSalaryNumberProperty()
     {
         return salaryNumber;
